b7adc0e0b8feeafe8c902325e3f1625444ff374d
jefferson.venancio <jefferson.venancio@dextra-sw.com>
2016-10-14 14:59:20 -0300

    git ignore

--- python/auth/controller.py
+++ python/auth/controller.py
@@ -7,14 +7,14 @@ auth = Blueprint('auth', __name__)
 CLIENT_ID = '905590247007-tmmromevhmghnve94lc1sqoh08itlhjf.apps.googleusercontent.com'
 CLIENT_SECRET = 'l-p5n0NQsBQNOl_naYVxgBFd'
 REDIRECT_URI = 'http://localhost:8080/api/auth/token'
-
+HD = 'dextra-sw.com'
 @auth.route('/')
 def hello():
-    if 'session_state' in session:
-        print session['session_state']
+    if 'barzinga_user' in session:
+        print session['barzinga_user']
         return redirect('/')
 
-    return redirect('https://accounts.google.com/AccountChooser?continue=https://accounts.google.com/o/oauth2/auth?scope%3Dhttps://www.googleapis.com/auth/userinfo.email%26response_type%3Dcode%26redirect_uri%3D'+REDIRECT_URI+'%26state%3Dsecurity_token%253D138r5719ru3e1%2526url%253Dhttp://barzinganow.appspot.com%26client_id%3D'+CLIENT_ID+'%26from_login%3D1%26as%3D-231db6e2ffa9ce49&btmpl=authsub&scc=1&oauth=1&hd=dextra-sw.com')
+    return redirect('https://accounts.google.com/AccountChooser?continue=https://accounts.google.com/o/oauth2/auth?scope%3Dhttps://www.googleapis.com/auth/userinfo.email%26response_type%3Dcode%26redirect_uri%3D'+REDIRECT_URI+'%26state%3Dsecurity_token%253D138r5719ru3e1%2526url%253Dhttp://barzinganow.appspot.com%26client_id%3D'+CLIENT_ID+'%26from_login%3D1%26as%3D-231db6e2ffa9ce49&btmpl=authsub&scc=1&oauth=1&hd='+HD)
 
 @auth.route('/token')
 def token():
@@ -32,7 +32,7 @@ def token():
     r = requests.get("https://www.googleapis.com/oauth2/v2/userinfo",
                      headers=authorization_header)
 
-    session['session_state'] = r.json()
+    session['barzinga_user'] = r.json()
 
     return redirect('/')
 
