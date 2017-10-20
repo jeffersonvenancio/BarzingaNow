22b4736b1243819a16db01eef0891ca354a30800
Diego Santos <diego.net10@hotmail.com>
2017-10-20 14:31:24 -0200

    A few adjustments at the login funtionality at main screen

--- android/app/src/main/java/com/barzinga/view/MainActivity.kt
+++ android/app/src/main/java/com/barzinga/view/MainActivity.kt
@@ -1,30 +1,52 @@
 package com.barzinga.view
 
 import android.app.Dialog
+import android.arch.lifecycle.ViewModelProviders
+import android.opengl.Visibility
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.view.View.*
 import android.widget.EditText
 import android.widget.Toast
 import com.barzinga.R
 import com.barzinga.customViews.BarzingaEditText
+import com.barzinga.manager.UserManager
 import com.barzinga.model.User
 import com.barzinga.restClient.RepositoryProvider
 import com.barzinga.util.ConvertObjectsUtil
+import com.barzinga.viewmodel.MainViewModel
 import io.reactivex.android.schedulers.AndroidSchedulers
 import io.reactivex.disposables.CompositeDisposable
 import io.reactivex.schedulers.Schedulers
 import kotlinx.android.synthetic.main.activity_main.*
+import android.view.KeyEvent.KEYCODE_BACK
+import android.content.DialogInterface
+import android.view.KeyEvent
 
-class MainActivity : AppCompatActivity() {
+
+class MainActivity : AppCompatActivity(), UserManager.DataListener {
+
+    lateinit var viewModel: MainViewModel
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        viewModel = ViewModelProviders.of(this).get(MainViewModel::class.java)
+        viewModel.setListener(this)
         
         llStart.setOnClickListener({
+
+            disableButton()
             logUser()
         })
+    }
+
+    private fun disableButton() {
+        btStartTitle.visibility = INVISIBLE
+        pbLoading.visibility = VISIBLE
+        llStart.isEnabled = false
     }
 
     private fun logUser() {
@@ -33,45 +55,49 @@ class MainActivity : AppCompatActivity() {
 
         dialog.findViewById<View>(R.id.rateApp).setOnClickListener {
             if (!(dialog.findViewById<View>(R.id.userEmail) as BarzingaEditText).text.toString().isEmpty()) {
-                //                    mainViewModel.logUser();
+                viewModel.logUser((dialog.findViewById<View>(R.id.userEmail) as BarzingaEditText).text.toString());
                 dialog.dismiss()
 
-                getProfile((dialog.findViewById<View>(R.id.userEmail) as BarzingaEditText).text.toString())
             } else {
                 (dialog.findViewById<View>(R.id.userEmail) as EditText).error = getString(R.string.invalid_user_error)
             }
         }
 
+        dialog.setOnKeyListener(object : DialogInterface.OnKeyListener {
+
+            override fun onKey(arg0: DialogInterface, keyCode: Int,
+                               event: KeyEvent): Boolean {
+
+                if (keyCode == KeyEvent.KEYCODE_BACK) {
+                    enableButton()
+
+                    dialog.dismiss()
+                }
+                return true
+            }
+        })
+
         dialog.show()
     }
 
-    fun getProfile(user: String) {
-
-        val compositeDisposable: CompositeDisposable = CompositeDisposable()
-        val repository = RepositoryProvider.provideUserRepository()
-
-        compositeDisposable.add(
-                repository.getProfile(user)
-                        .observeOn(AndroidSchedulers.mainThread())
-                        .subscribeOn(Schedulers.io())
-                        .subscribe ({
-                            result ->
-                            onUserRequestSuccess(result)
-                        }, { error ->
-                            error.printStackTrace()
-                            onUserRequestFailure()
-                        })
-        )
-
+    override fun onLogInSuccess(user: User) {
+        OptionsActivity.start(this, ConvertObjectsUtil.getStringFromObject(user))
+        finish()
     }
 
-    private fun onUserRequestFailure() {
-        Toast.makeText(this, "Falha no login! Por favor tente novamente.", Toast.LENGTH_SHORT).show();
+    override fun onLogInFailure() {
+        Toast.makeText(this, getString(R.string.login_failure), Toast.LENGTH_SHORT).show();
+
+        enableButton()
     }
 
-    private fun onUserRequestSuccess(result: User?) {
-        if (result != null){
-            OptionsActivity.start(this, ConvertObjectsUtil.getStringFromObject(result))
-        }
+    private fun enableButton() {
+        btStartTitle.visibility = VISIBLE
+        pbLoading.visibility = INVISIBLE
+
+        llStart.isEnabled = true
     }
+
+
+
 }
--- android/app/src/main/java/com/barzinga/viewmodel/MainViewModel.kt
+++ android/app/src/main/java/com/barzinga/viewmodel/MainViewModel.kt
@@ -1,36 +1,39 @@
 package com.barzinga.viewmodel
 
 import com.barzinga.manager.UserManager
+import com.barzinga.model.User
+import android.app.Application
+import android.arch.lifecycle.AndroidViewModel
 
 /**
  * Created by diego.santos on 03/10/17.
  */
-class MainViewModel(internal var mListener: DataListener) : ViewModel, UserManager.DataListener {
+class MainViewModel(application: Application) : AndroidViewModel(application), UserManager.DataListener {
     internal var userManager: UserManager
+    var mListener: UserManager.DataListener? = null
 
     init {
 
         userManager = UserManager(this)
     }
 
-    override fun onViewDestroy() {
-        userManager.onViewDestroy()
+    fun setListener(listener: UserManager.DataListener){
+        mListener = listener
     }
 
-    fun logUser() {
-        userManager.logIn()
+//    override fun onViewDestroy() {
+//        userManager.onViewDestroy()
+//    }
+
+    fun logUser(user: String) {
+        userManager.logIn(user)
     }
 
-    override fun onLogInSuccess() {
-        mListener.onLogInSuccess()
+    override fun onLogInSuccess(user: User) {
+        mListener?.onLogInSuccess(user)
     }
 
     override fun onLogInFailure() {
-        mListener.onLogInFailure()
-    }
-
-    interface DataListener {
-        fun onLogInSuccess()
-        fun onLogInFailure()
+        mListener?.onLogInFailure()
     }
 }
