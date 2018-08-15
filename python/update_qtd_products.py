8e83e6e2ab37bdd674dfe24286386e87eebf80e5
Jefferson <jefferson.venancio@dextra-sw.com>
2018-08-15 10:28:24 -0300

    Script de add products

--- python/update_qtd_products.py
+++ python/update_qtd_products.py
@@ -8,8 +8,8 @@ with open('update_products.csv', 'rb') as data:
     headers = {'content-type' : 'application/x-www-form-urlencoded', 'Bearer' : 'Token Diego'}
     params = {'sessionKey': '9ebbd0b25760557393a43064a92bae539d962103', 'format': 'xml', 'platformId': 1}
     for line in reader:
-        # url = "https://v7-dot-barzinganow.appspot.com/api/product/"
-        url = "http://localhost:8080/api/product/"
+        url = "https://v7-dot-barzinganow.appspot.com/api/product/"
+        # url = "http://localhost:8080/api/product/"
         url += line[0]
         url += "/add"
         data = {"quantity": str(line[2])}
