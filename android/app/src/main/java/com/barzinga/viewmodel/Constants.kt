40a9da4e1dbe1648ec11321371cadcf0e6c0cfb8
Diego Santos <diego.net10@hotmail.com>
2018-09-28 17:46:11 -0300

    Changed rfid url

--- android/app/src/main/java/com/barzinga/viewmodel/Constants.kt
+++ android/app/src/main/java/com/barzinga/viewmodel/Constants.kt
@@ -5,7 +5,7 @@ package com.barzinga.viewmodel
  */
 object Constants{
     val BASE_URL: String = "http://barzinganow.appspot.com/api/"
-    val BASE_RFID_URL: String = "http://d2543868.ngrok.io"
+    val BASE_RFID_URL: String = "http://192.168.8.139:8765"
     val USER_EXTRA: String = "USER_EXTRA"
     val TRANSACTION_EXTRA: String = "TRANSACTION_EXTRA"
     val CHECKOUT_REQUEST: Int = 101
