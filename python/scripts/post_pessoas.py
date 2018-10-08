4f1063cb9d6cb07f0eb28340bc21d47f7315e2ac
Jefferson <jefferson.venancio@dextra-sw.com>
2018-10-08 14:33:55 -0300

    ajustando scripts de update

---
+++ python/scripts/post_pessoas.py
@@ -0,0 +1,14 @@
+import csv, json
+import requests
+
+X = []
+Y = []
+with open('pessoas.csv', 'rb') as data:
+    reader = csv.reader(data, delimiter=',')
+    headers = {'content-type' : 'application/x-www-form-urlencoded'}
+    params = {'sessionKey': '9ebbd0b25760557393a43064a92bae539d962103', 'format': 'xml', 'platformId': 1}
+    for line in reader:
+        # url = "http://barzinganow.appspot.com/api/user/"
+        url = "http://localhost:8080/api/user/"
+        data = {"name":line[0],"email":line[1]}
+        print requests.post(url, data=data, headers=headers, params=params)
